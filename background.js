ad57fff900daf7b815a2bd24e8f3d7d56a64b94d
soheilpaper <soheil_paper@yahoo.com>
2020-01-29 19:57:15 +0330

    first commit2

---
+++ background.js
@@ -0,0 +1,31 @@
+const SCIHUB_URL = 'scihub_url';
+const DEFAULT_SCIHUB_URL = 'https://scihub.bban.top/';
+const CURRENT_URL = "current_url";
+let storage = browser.storage.sync;
+let config = {};
+
+config[SCIHUB_URL] = DEFAULT_SCIHUB_URL;
+
+function openSciHubPdf() {
+  // Get current path
+  browser.tabs.query({currentWindow: true, active: true})
+    .then((tabs) => {
+      if(tabs.length > 0) {
+        // Get the CURRENT_URL configuration
+        storage.get(config, function (config) {
+          current_active_url = tabs[0].url
+          browser.tabs.create({
+            url: config[SCIHUB_URL] + '/' + current_active_url,
+          });
+        })
+      }
+  })
+}
+
+function updateCurrentPage(details) {
+  let config = {};
+  config[CURRENT_URL] = details.url;
+  return storage.set(config);
+}
+
+browser.browserAction.onClicked.addListener(openSciHubPdf);
